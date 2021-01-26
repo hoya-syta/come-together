1a7ed54d424117888a0b8c4b5746dde0c7dceb55
hoya-syta <73057535+hoya-syta@users.noreply.github.com>
2021-01-26 21:52:29 +0900

    仮・年齢ファイル

---
+++ app/models/age.rb
@@ -0,0 +1,18 @@
+class DeliveryZone < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '選択してください' },
+    { id: 2, name: '20歳未満' },
+    { id: 3, name: '20-29歳' },
+    { id: 4, name: '30-39歳' },
+    { id: 5, name: '40-49歳' },
+    { id: 6, name: '50-59歳' },
+    { id: 7, name: '60-69歳' },
+    { id: 8, name: '70-79歳' },
+    { id: 9, name: '80歳以上' },
+    { id: 10, name: 'その他' },
+  ]
+
+include ActiveHash::Associations
+  belongs_to :user
+end
+
