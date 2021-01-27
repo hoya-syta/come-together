706fe6730df05c4f0c0ca31e84169dbb872277db
hoya-syta <73057535+hoya-syta@users.noreply.github.com>
2021-01-27 21:53:58 +0900

    仮・性別ファイル

---
+++ app/models/gender.rb
@@ -0,0 +1,11 @@
+class DeliveryZone < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '選択してください' },
+    { id: 2, name: '男性' },
+    { id: 3, name: '女性' },
+  ]
+
+include ActiveHash::Associations
+  belongs_to :user
+end
+
