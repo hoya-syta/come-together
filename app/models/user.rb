da915efed10a5b37aca3e66d97ce3376c99fe76d
hoya-syta <73057535+hoya-syta@users.noreply.github.com>
2021-02-10 23:50:52 +0900

    user バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,21 @@ class User < ApplicationRecord
     belongs_to :perfecture
     belongs_to :profession
 
+    with_options presence: true do
+      validates :nickname
+      validates :birthday
+      validates :password, length: { minimum: 6 },
+                           format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+  
+      with_options format: { with: /\A[ぁ-んァ-ン一-龥]/ } do
+        validates :last_name
+        validates :first_name
+      end
+  
+      with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
+        validates :last_name_hurigana
+        validates :first_name_hurigana
+      end
+    end
 
 end
