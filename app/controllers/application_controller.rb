2e95fedad8bd5bc4307a03239389b115fe891be4
hoya-syta <73057535+hoya-syta@users.noreply.github.com>
2021-02-13 17:30:13 +0900

    エラーメッセージ表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
-end
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+ 
+ 
+  def configure_permitted_parameters 
+   devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :gender_id, :password, :prefecture_id, :profession_id, :birthday, :age_id])
+  end
+
+ end
+ 
