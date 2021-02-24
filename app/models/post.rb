a30aa7e6d81e8f72c5c136d2d039f48292b67174
hoya-syta <73057535+hoya-syta@users.noreply.github.com>
2021-02-24 17:21:07 +0900

    メッセージモデル、バリデーション記述

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,4 +8,5 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :users, through: :user_posts
   has_one_attached :image
+  has_many :messages, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -36,6 +36,7 @@ class User < ApplicationRecord
 
     has_many :user_posts
     has_many :posts, through: :user_posts
+    has_many :messages
     
     
 
