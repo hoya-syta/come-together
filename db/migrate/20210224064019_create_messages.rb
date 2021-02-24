e3791a6457cd081ca0bf96eccee9d187e2796c5d
hoya-syta <73057535+hoya-syta@users.noreply.github.com>
2021-02-24 16:16:17 +0900

    投稿エラーメッセージ 表記

--- db/migrate/20210224064019_create_messages.rb
+++ db/migrate/20210224064019_create_messages.rb
@@ -1,6 +1,9 @@
 class CreateMessages < ActiveRecord::Migration[6.0]
   def change
     create_table :messages do |t|
+      t.string     :content,  null: false
+      t.references :user,     foreign_key: true
+      t.references :post,     foreign_key: true
 
       t.timestamps
     end
