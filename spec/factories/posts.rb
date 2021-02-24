6ea75e37feceb8c0f0861c5c268e26a0ce7342fb
hoya-syta <73057535+hoya-syta@users.noreply.github.com>
2021-02-24 16:49:37 +0900

    テストコード実装

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :post do
-    
+    title  { 'aaa' }
+    detail { 'test@mail.com' }
+    association :user
+
+    after(:build) do |post|
+      post.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+
   end
 end
