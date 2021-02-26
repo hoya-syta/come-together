3d5c5caf65bfc739f54a8ed7666b8f8591e25573
hoya-syta <73057535+hoya-syta@users.noreply.github.com>
2021-02-26 17:23:01 +0900

    メッセージ機能テストコード

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe Message do
+    before do
+      @message = FactoryBot.build(:message)
+    end
+
+    describe 'メッセージ投稿の可否' do
+      context '投稿ができる場合' do
+        it '全ての項目が存在すれば登録できる。' do
+          expect(@message).to be_valid
+        end
+      end
+
+      context '投稿ができない場合' do
+        it 'contentが空では登録できない' do
+          @message.content = ''
+          @message.valid?
+          expect(@message.errors.full_messages).to include("メッセージを入力してください")
+        end
+      end
+    end
+  end
 end
