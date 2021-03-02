23fbfb54e13eb04d3d1600e78aecdddb8900685c
hoya-syta <73057535+hoya-syta@users.noreply.github.com>
2021-03-02 16:40:04 +0900

    indexテストコード実装

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -8,15 +8,19 @@ describe PostsController, type: :request do
   describe 'GET #index' do
     it 'indexアクションにリクエストすると正常にレスポンスが返ってくる' do 
       get root_path
-      binding.pry
+      expect(response.status).to eq 200
     end
-    it 'indexアクションにリクエストするとレスポンスに投稿済みの記事のテキストが存在する' do 
+    it 'indexアクションにリクエストするとレスポンスに投稿済みの記事のタイトルが存在する' do
+      get root_path
+      expect(response.body).to include(@post.title)
     end
-    it 'indexアクションにリクエストするとレスポンスに投稿済みの記事の画像URLが存在する' do 
-    end
-    it 'indexアクションにリクエストするとレスポンスに投稿検索フォームが存在する' do 
+    it 'indexアクションにリクエストするとレスポンスに投稿済みの記事の詳細が存在する' do 
+      get root_path
+      expect(response.body).to include(@post.detail)
     end
     it 'indexアクションにリクエストするとレスポンスに投稿ボタンが存在する' do 
+      get root_path
+      expect(response.body).to include('♪投稿する♪')
     end
   end
 end
